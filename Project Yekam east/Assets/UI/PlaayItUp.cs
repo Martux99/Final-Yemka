a6888fbb4c36e6e94035e5d6a5b8e7787a40904c
Martux99 <34321961+Martux99@users.noreply.github.com>
2018-05-20 04:10:48 -0500

    fg

---
+++ Project Yekam east/Assets/UI/PlaayItUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlaayItUp : MonoBehaviour {
+
+    public GameObject Panel;
+
+    public void SelectItUp()
+    {
+        Panel.SetActive(true);
+    }
+
+}
